2791a2fb6f1d624f232e756536776bb8a3ab0b5a
hareefaree <marissa.vazhappilly@gmail.com>
2023-11-30 14:49:42 -0500

    Linear event triggers initial set up

---
+++ Assets/Scripts/CountTracker/CountCapturer.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CountCapturer : MonoBehaviour
+{
+    public int timesEntered;
+    // Start is called before the first frame update
+    void Start()
+    {
+        timesEntered = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "player")
+        {
+            timesEntered = timesEntered + 1;
+        }
+    }
+}
