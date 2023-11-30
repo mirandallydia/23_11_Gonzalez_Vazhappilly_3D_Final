5734e3673ca64942a27738f90bbc91dfce0a8973
Magicdadjokes <mirandallydia@gmail.com>
2023-11-30 14:33:03 -0500

    you can pick things up but not put them down :(

---
+++ Assets/Scripts/ObjectGrabbable.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectGrabbable : MonoBehaviour
+{
+    private Rigidbody objectRigidBody;
+    private Transform objectGrabPointTransform;
+    private void Awake()
+    {
+        objectRigidBody = GetComponent<Rigidbody>();
+    }
+    public void Grab(Transform objectGrabPointTransfrom) 
+    {
+        this.objectGrabPointTransform = objectGrabPointTransfrom;
+        objectRigidBody.useGravity = false;
+    }
+
+   
+    private void FixedUpdate()
+    {
+        if (objectGrabPointTransform != null)
+        {
+            float lerpSpeed = 10f;
+            Vector3 newPosition = Vector3.Lerp(transform.position, objectGrabPointTransform.position, Time.deltaTime * lerpSpeed);
+            objectRigidBody.MovePosition(newPosition);
+        }
+    }
+}
